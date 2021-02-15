e62a4098d349051b5446941152996ebb69603c40
maraesa100 <siobhanmckenzie90@gmail.com>
2021-02-15 16:17:13 +0000

    Project Complete

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 import { render } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import store from './app/store'
 import App from './App'
+import { selectMonthly } from './features/subscription/subscriptionSlice'
 
-test('renders learn react link', () => {
+test('renders "take control of your investments" string', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   )
-
   expect(getByText('Take control of your investments')).toBeInTheDocument()
 })
+
+//  Further work: Redux tests, and unit testing for helper functions
+// (would move cart calculations out into helper functions, for example)
